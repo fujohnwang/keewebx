20d70eec5a6b4905d6cf7a5038c3a1b00af6a376
afoo <i@afoo.me>
2025-06-27 21:11:45 +0800

    add content

--- src/main/scala/com/keevol/keewebx/templating/Partial.scala
+++ src/main/scala/com/keevol/keewebx/templating/Partial.scala
@@ -7,5 +7,12 @@ import io.vertx.core.json.JsonObject
  * refer to project /Users/fq/workspace.keevol/KeevolAdminWebStarter for demo usage.
  */
 object Partial {
+  /**
+   * 这个是从模板创建Content占位符内容， 但其实也可以直接实现Content实现类作为模板参数。
+   *
+   * @param templatePath
+   * @param context
+   * @return
+   */
   def apply(templatePath: String, context: JsonObject): Content = Jte.createContent(templatePath, context)
 }
